00d987f76b9a9d0eecd53ac69ea165d1ef756efe
CPSDC <CPSDC@DESKTOP-7T1PPNB>
2018-06-01 15:13:19 +0800

    的

--- springcloud-service-consumer/src/main/java/com/example/consumer/ConsumeService.java
+++ springcloud-service-consumer/src/main/java/com/example/consumer/ConsumeService.java
@@ -3,34 +3,29 @@ package com.example.consumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
-public class DcController {
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
-	@Autowired
-	LoadBalancerClient loadBalancerClient;
+@Service
+public class ConsumeService {
+
 	@Autowired
 	RestTemplate restTemplate;
 	@Autowired
-	ConsumeService consumeService;
+	LoadBalancerClient loadBalancerClient;
 	
-	
-	@GetMapping("/consumer")
-	public String dc() {
+	@HystrixCommand(fallbackMethod = "fallback")
+	public String consumer2() throws InterruptedException {
+		Thread.sleep(6000L);
 		ServiceInstance serviceInstance = loadBalancerClient.choose("service-provide-01");
 		String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/getUser/2";
 		System.out.println(url);
 		return restTemplate.getForObject(url, String.class);
 	}
 
-	@GetMapping("/consumer2")
-	public String consumer2() throws InterruptedException {
-		String s = consumeService.consumer2();
-		return  s;
+	public String fallback() {
+		return "fallbck123";
 	}
-	
-	
 }

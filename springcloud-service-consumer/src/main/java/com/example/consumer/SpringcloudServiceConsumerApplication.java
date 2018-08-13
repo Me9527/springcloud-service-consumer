6dbccdd447e411f8a443148f588dfa912cea78cd
CPSDC <CPSDC@DESKTOP-7T1PPNB>
2018-08-13 16:32:40 +0800

    ddd

--- springcloud-service-consumer/src/main/java/com/example/consumer/SpringcloudServiceConsumerApplication.java
+++ springcloud-service-consumer/src/main/java/com/example/consumer/SpringcloudServiceConsumerApplication.java
@@ -2,10 +2,12 @@ package com.example.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SpringcloudServiceConsumerApplication {
